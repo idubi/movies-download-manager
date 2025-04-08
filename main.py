bb501ca039366e60168f0f6c46e5f54ebe60c700
DESKTOP-82998RE\idobi <ido.bistry@gmail.com>
2025-04-08 03:04:40 +0300

    after creation of kafka docker and before seperating to microseervices to be handles throought kafka

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ from src.cookies_authentication import (
     save_cookies_to_file,
 )
 from src.movie_utils import download_videos_async
+from src.config_manager import read_links_file
 
 
 browser_cookies_file_path = "./resources/raw-cookies.txt"
 cookies_file_path = "./resources/cookies.txt"
-links_file_path = "./resources/links-capsule04.txt"
+links_file_path = "./resources/links-tests.txt"
 max_concurrent_downloads = 5
 
 
 if __name__ == "__main__":
+    # Step 1: Create cookies file
     cookies_collection = create_cookies_from_browser_cookies(browser_cookies_file_path)
     save_cookies_to_file(cookies_collection, cookies_file_path)
     print(f"Cookies file created at: {cookies_file_path}")
-    # download_videos(cookies_file_path, "./resources/links.txt")
+
+    # Step 2: Read links file
+    tasks = read_links_file(links_file_path)
+    print(f"Loaded {len(tasks)} tasks from links file.")
+
+    # Step 3: Start downloads
     asyncio.run(
-        download_videos_async(
-            cookies_file_path, links_file_path, max_concurrent_downloads
-        )
+        download_videos_async(cookies_file_path, tasks, max_concurrent_downloads)
     )
+
+    print("All downloads completed.")
